863b4e717b7e38517860e2d4c20de3d6b83a6f12
OPD51 <tomalapatryk172@gmail.com>
2024-10-31 09:49:37 +0100

    data en app config

---
+++ Barroc_intens/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+namespace Barroc_Intensive.Data
+{
+	internal class AppDbContext : DbContext
+	{
+
+		public DbSet<User> Users { get; set; }
+
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseMySql(
+				ConfigurationManager.ConnectionStrings["AppDbContext"].ConnectionString,
+				ServerVersion.Parse("5.7.33")
+			);
+		}
+
+	}
+}
---
+++ Barroc_intens/Data/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barroc_Intensive.Data
+{
+	internal class User
+	{
+	}
+}
